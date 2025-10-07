8b596027298fbb83ea8b50bea8cf4b2381bb0127
Eboma <96636027+Eboma@users.noreply.github.com>
2025-10-07 20:36:45 +0100

    edited

--- main.py
+++ main.py
@@ -186,6 +186,10 @@ def send_reset_email(user_mail, token):
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
